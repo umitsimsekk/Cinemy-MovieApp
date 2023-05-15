a9da6d15c528a912e1c0635fc26bebc7ab163caf
Ümit Şimşek <umitsimsek002@gmail.com>
2023-05-15 17:01:53 +0300

    Initial Commit

---
+++ Cinemy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinemy
+//
+//  Created by Ümit Şimşek on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
