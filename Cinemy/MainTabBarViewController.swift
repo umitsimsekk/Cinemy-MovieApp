85b1b2e7d84892c787cd0a4703ec79f85c34ceb5
Ümit Şimşek <umitsimsek002@gmail.com>
2023-05-15 17:27:26 +0300

    main tabbar created

--- Cinemy/MainTabBarViewController.swift
+++ Cinemy/MainTabBarViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
 
 
